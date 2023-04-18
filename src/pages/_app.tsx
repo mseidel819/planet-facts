16c9a4740eb25e1220be3b85656c1e207381c2c4
Matt Seidel <mseidel819@gmail.com>
2023-04-18 14:48:51 -0500

    /

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import NavBar from "../components/nav-bar/nav-bar.component";
-import "@/styles/globals.scss";
+import "../styles/globals.scss";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
