66549fe7c3edc019bce8c30bc78c626a616fd11c
Matt Seidel <mseidel819@gmail.com>
2023-04-18 15:20:03 -0500

    next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import NavBar from "../components/nav-bar/nav-bar.component";
-import "../styles/globals.scss";
+
 import type { AppProps } from "next/app";
+
 import { Antonio } from "next/font/google";
 import { League_Spartan } from "next/font/google";
+import "../styles/globals.scss";
 
-const antonio = Antonio({ subsets: ["latin"] });
-const leagueSpartan = League_Spartan({ subsets: ["latin"] });
+const antonio = Antonio({
+  variable: "--header-font",
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
+const leagueSpartan = League_Spartan({
+  variable: "--body-font",
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <main className={`${antonio.className}  ${leagueSpartan.className}`}>
